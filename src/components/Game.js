cac1b790b79fbddd166bf7fa05f20b3a3c9a0a66
siva010928 <siva010928@gmail.com>
2024-05-19 13:47:19 +0530

    implemented Game Component bare minmum code at first and refactored this component to pass the test.

---
+++ src/components/Game.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Board from "./Board";
+
+export default function Game() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove];
+    const xIsNext = currentMove % 2 === 0;
+
+    function handlePlay(nextSquares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    function jumpTo(nextMove) {
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((_, move) => {
+        const description = move ? `Go to move #${move}` : 'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)} data-testid={`move-${move}`}>{description}</button>
+            </li>
+        );
+    });
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
+            <div className="game-info">
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+}
